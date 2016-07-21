a6e98aa98ac036fe556c46acededf745b95402ed
tobby <tobby@tobby.eu>
2016-07-21 22:32:00 +0200

    Unify change-state so everything could be changed in one heartbeat interval

--- src/enums.hpp
+++ src/enums.hpp
@@ -2,6 +2,6 @@
 #define ENUMS_H
 
 enum class Feature_Type {Input_Device, Output_Device, Hook};
-enum class Device_Status_Response {OK, name_change, heartbeat_change};
+enum class Device_Status_Response {OK, changes_to_apply};
 
 #endif // ENUMS_H
