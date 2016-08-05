0eb9ca5907a491736107f00e47f5fde1f787449e
tobby <tobby@tobby.eu>
2016-08-05 23:35:48 +0200

    Add empty Qt gui project and merge it into api project

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include "api.hpp"
+#include "mainwindow.hpp"
 #include "ros/ros.h"
+#include <QApplication>
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "TobbyAPI");
   ros::NodeHandle nh;
   Api api(&nh);
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.show();
+
+  return a.exec();
   api.Run();
 
   return 0;
