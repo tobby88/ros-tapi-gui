8b3a8e66c64cd1db4eaa8d5ce1b8791e92c2147f
tobby <tobby@tobby.eu>
2016-08-10 00:04:23 +0200

    Display some connected devices (not very flexible :-/)

--- src/apideviceswrapper.cpp
+++ src/apideviceswrapper.cpp
@@ -13,7 +13,7 @@ ApiDevicesWrapper::~ApiDevicesWrapper() {}
 int ApiDevicesWrapper::rowCount(const QModelIndex&) const
 {
   //return api->GetDevices().size();
-  return 3;
+  return 10;
 }
 
 int ApiDevicesWrapper::columnCount(const QModelIndex&) const { return 3; }
@@ -26,43 +26,39 @@ QVariant ApiDevicesWrapper::data(const QModelIndex& index, int role) const
   switch (role)
   {
   case Qt::DisplayRole:
-    if (row == 0 && col == 1)
-      return QString("<--left");
-    if (row == 1 && col == 1)
-      return QString("right-->");
-
-    //return QString("Row%1, Column%2").arg(row + 1).arg(col + 1);
-    return QString::number(api->GetDevices().size());
-    break;
-  case Qt::FontRole:
-    if (row == 0 && col == 0) // change font only for cell(0,0)
+    if (row < api->GetDevices().size())
     {
-      QFont boldFont;
-      boldFont.setBold(true);
-      return boldFont;
+      map<string, Device> devices = api->GetDevices();
+      map<string, Device>::iterator it = devices.begin();
+      for (int i = 0; i < row; i++)
+      {
+        it++;
+      }
+      return QString::fromStdString(it->second.getName());
     }
     break;
-  case Qt::BackgroundRole:
+    /*case Qt::FontRole:
+      if (row == 0 && col == 0) // change font only for cell(0,0)
+      {
+        QFont boldFont;
+        boldFont.setBold(true);
+        return boldFont;
+      }
+      break;
+    case Qt::BackgroundRole:
 
-    if (row == 1 && col == 2) // change background only for cell(1,2)
-    {
-      QBrush redBackground(Qt::red);
-      return redBackground;
-    }
-    break;
-  case Qt::TextAlignmentRole:
+      if (row == 1 && col == 2) // change background only for cell(1,2)
+      {
+        QBrush redBackground(Qt::red);
+        return redBackground;
+      }
+      break;
+    case Qt::CheckStateRole:
 
-    if (row == 1 && col == 1) // change text alignment only for cell(1,1)
-    {
-      return Qt::AlignRight + Qt::AlignVCenter;
-    }
-    break;
-  case Qt::CheckStateRole:
-
-    if (row == 1 && col == 0) // add a checkbox to cell(1,0)
-    {
-      return Qt::Checked;
-    }
+      if (row == 1 && col == 0) // add a checkbox to cell(1,0)
+      {
+        return Qt::Checked;
+      }*/
   }
   return QVariant();
 }
