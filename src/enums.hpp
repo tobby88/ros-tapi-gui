f1681b0be3a113fc3d6ec6a62724289daf1b3df6
tobby <tobby@tobby.eu>
2016-08-22 02:37:30 +0200

    Refactoring:  Sort

--- src/enums.hpp
+++ src/enums.hpp
@@ -1,16 +1,18 @@
 #ifndef ENUMS_H
 #define ENUMS_H
 
-enum class DeviceType
-{
-  SenderDevice,
-  ReceiverDevice,
-};
 enum class DeviceStatusResponse
 {
   OK,
   Error
 };
+
+enum class DeviceType
+{
+  SenderDevice,
+  ReceiverDevice,
+};
+
 enum class FeatureType
 {
   Images,
